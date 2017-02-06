e7176026c2822129aee749ce80b2ce797751cf88
yanlong <yanlongzh@163.com>
2017-02-06 23:25:04 +0800

    二月六日 bugly 提交

---
+++ app/src/main/java/com/example/hui/mynews/Application/BaseApplication.java
@@ -0,0 +1,10 @@
+package com.example.hui.mynews.Application;
+
+import android.app.Application;
+
+/**
+ * Created by yanlongzh on 2017/2/6.
+ */
+
+public class BaseApplication extends Application {
+}
